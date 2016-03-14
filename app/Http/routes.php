ee37d60214ccd10087b0df065c744e54024d8e7e
khunemz <khunemz@gmail.com>
2016-03-14 20:07:00 +0700

    Create user , password reset , add column user_id,Create Article & User controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,5 +27,7 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+
+    Route::resource('users', 'UserController');
+    Route::resource('users.articles', 'ArticleController');
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.master')
+@extends('layouts.master')
 @section('content')
     <h4>Bootstraptest</h4>
 @endsection
